3da3b081555da4c93726f3101e2ab5f4a68119d0
nsbbezerra <nsbbezerra@hotmail.com>
2020-05-27 16:37:01 -0300

    adjust url

--- src/configs/index.js
+++ src/configs/index.js
@@ -1,5 +1,5 @@
 module.exports = {
   db_connection:
     "mongodb://palmieri:natan210290@ds061681.mlab.com:61681/palmieri",
-  photo_url: "https://palmieri-api.herokuapp.com/",
+  photo_url: "https://palmieri-api.herokuapp.com",
 };
