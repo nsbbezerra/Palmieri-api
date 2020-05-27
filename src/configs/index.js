c37b3a92206e677dc2b6f05d9dde1f821d2581ef
nsbbezerra <nsbbezerra@hotmail.com>
2020-05-27 16:29:18 -0300

    add Procfile

--- src/configs/index.js
+++ src/configs/index.js
@@ -1,5 +1,5 @@
 module.exports = {
   db_connection:
     "mongodb://palmieri:natan210290@ds061681.mlab.com:61681/palmieri",
-  photo_url: "https://palmieri-api.herokuapp.com:3333",
+  photo_url: "http://localhost:3333",
 };
--- src/index.js
+++ src/index.js
@@ -1,21 +1,21 @@
-require('dotenv').config();
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const path = require('path');
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const path = require("path");
 const app = express();
 
-const routes = require('./routes/index');
+const routes = require("./routes/index");
 
 app.use(cors());
 app.use(express.json());
-app.use(bodyParser.json({ limit: '20mb' }));
-app.use(bodyParser.urlencoded({ limit: '20mb', extended: true }));
-app.use('/img', express.static(path.resolve(__dirname, '..', 'uploads')));
+app.use(bodyParser.json({ limit: "20mb" }));
+app.use(bodyParser.urlencoded({ limit: "20mb", extended: true }));
+app.use("/img", express.static(path.resolve(__dirname, "..", "uploads")));
 app.use(routes);
 
-const port = process.env.APLICATION_PORT || 3333;
+const port = process.env.PORT || 3333;
 
-app.listen(port, function() {
-    console.log('Servidor Ativo na Porta', port);
+app.listen(port, function () {
+  console.log("Servidor Ativo na Porta", port);
 });
