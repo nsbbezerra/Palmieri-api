61a671edf05a70a22a71f207ae01cb8d0952250e
nsbbezerra <nsbbezerra@hotmail.com>
2020-07-14 18:38:06 -0300

    connect with desktop app

--- src/configs/index.js
+++ src/configs/index.js
@@ -1,5 +1,5 @@
 module.exports = {
   db_connection:
     "mongodb://palmieriunifor01:natan210290@mongodb.palmieriuniformes.com:27017/palmieriunifor01",
-  photo_url: "http://localhost:3333/img",
+  photo_url: "http://palmieriuniformes.com:21108/img",
 };
