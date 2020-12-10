8e3b9d7e47d64900189db34ca3830a7b66deea13
Natanael Bezerra <nsbbezerra@hotmail.com>
2020-12-10 14:11:12 -0300

    finalize all

--- src/configs/index.js
+++ src/configs/index.js
@@ -2,6 +2,6 @@ module.exports = {
   db_connection:
     "mongodb://palmieriunifor01:natan210290@mongo71-farm76.kinghost.net:27017/palmieriunifor01",
   mlab: "mongodb://palmieri:natan210290@ds061681.mlab.com:61681/palmieri",
-  //photo_url: "http://palmieriuniformes.nodejs7602.kinghost.net:21489/img",
-  photo_url: "http://localhost:21489/img",
+  photo_url: "http://palmieriuniformes.nodejs7602.kinghost.net:21489/img",
+  //photo_url: "http://localhost:21489/img",
 };
--- src/controllers/siteController.js
+++ src/controllers/siteController.js
@@ -1,9 +1,9 @@
 const Category = require("../models/categrory");
 const Product = require("../models/products");
-const Catalog = require("../models/portifilio");
 const Comments = require("../models/depoiments");
-const Models = require("../models/modelagem");
-const Tabels = require("../models/tabelas");
+const Catalog = require("../models/portifilio");
+const Tables = require("../models/tabelas");
+const Modelagem = require("../models/modelagem");
 const config = require("../configs/index");
 const express = require("express");
 const router = express.Router();
@@ -21,9 +21,75 @@ router.get("/home", async (req, res) => {
 
     const urlPhoto = config.photo_url;
 
+    return res.status(200).json({
+      category,
+      products,
+      productsFooter,
+      comments,
+      urlPhoto,
+    });
+  } catch (error) {
+    const erro = {
+      message: "Erro ao buscar as informações",
+      type: error.message,
+    };
+    return res.status(400).json(erro);
+  }
+});
+
+router.get("/productPage", async (req, res) => {
+  try {
+    const category = await Category.find({ active: true }).select("name");
+    const products = await Product.find({ active: true }).select(
+      "name thumbnail description imageDescription category"
+    );
+    const urlPhoto = config.photo_url;
+    return res.status(200).json({ category, products, urlPhoto });
+  } catch (error) {
+    const erro = {
+      message: "Erro ao buscar as informações",
+      type: error.message,
+    };
+    return res.status(400).json(erro);
+  }
+});
+
+router.get("/catalogPage", async (req, res) => {
+  try {
+    const products = await Product.find({ active: true });
+    const catalogs = await Catalog.find({ active: true });
+    const tables = await Tables.find();
+    const modelage = await Modelagem.find();
+    const urlPhoto = config.photo_url;
     return res
       .status(200)
-      .json({ category, products, productsFooter, comments, urlPhoto });
+      .json({ products, urlPhoto, catalogs, tables, modelage });
+  } catch (error) {
+    const erro = {
+      message: "Erro ao buscar as informações",
+      type: error.message,
+    };
+    return res.status(400).json(erro);
+  }
+});
+
+router.get("/findCategory", async (req, res) => {
+  try {
+    const category = await Category.find({ active: true }).select("name");
+    return res.status(200).json({ category });
+  } catch (error) {
+    const erro = {
+      message: "Erro ao buscar as informações",
+      type: error.message,
+    };
+    return res.status(400).json(erro);
+  }
+});
+
+router.get("/findAllProducts", async (req, res) => {
+  try {
+    const products = await Product.find({ active: true }).select("name");
+    return res.status(200).json({ products });
   } catch (error) {
     const erro = {
       message: "Erro ao buscar as informações",
